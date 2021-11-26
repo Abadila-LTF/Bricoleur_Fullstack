573625092be8f4191615eb2c4d9c5fdb67ace831
Abadila Alaktif <53411526+Abadila-LTF@users.noreply.github.com>
2021-11-26 01:04:52 +0100

    Update config.py

--- brikoleur_backend/brikoleur_backend-main/brikoleur/config.py
+++ brikoleur_backend/brikoleur_backend-main/brikoleur/config.py
@@ -1,15 +1,15 @@
 # config.py
 
 #dev
-MYSQL_HOST = 'database-1.cdyguxgp6lky.eu-west-3.rds.amazonaws.com' 
-MYSQL_USERNAME = 'reda'
-MYSQL_PASSWORD = 'ef7zgHFRoln9i8dRLWUS'
+MYSQL_HOST = 'XXXXXXXXXXX' 
+MYSQL_USERNAME = 'XXXXXXXX'
+MYSQL_PASSWORD = 'XXXXXXXX'
 MYSQL_DATABASE = 'brikoleur'
 MYSQL_PORT = '3307'
 
 SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://'+MYSQL_USERNAME+':'+MYSQL_PASSWORD+'@'+MYSQL_HOST+':'+MYSQL_PORT+'/'+MYSQL_DATABASE
 SERVER_HOST = '0.0.0.0'
 KEY_JWT = 'mySecret'  # Prod
-MAIL_SERVER = 'smtp.gmail.com'
-MAIL_USERNAME = 'redaabdou49@gmail.com'
-MAIL_PASSWORD = 'hblnjyhikjcddhli'
+MAIL_SERVER = 'XXXXXXXXXXXX'
+MAIL_USERNAME = 'XXXXXXX'
+MAIL_PASSWORD = 'XXXXXXXXXXX'
